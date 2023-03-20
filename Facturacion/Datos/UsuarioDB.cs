8419a3760d4aff60aac42d6a932c75c7fb9eef5b
NALS1989 <91510346+NALS1989@users.noreply.github.com>
2023-03-20 12:01:45 -0600

    clase db usuario

---
+++ Facturacion/Datos/UsuarioDB.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class UsuarioDB
+    {
+        string cadena = "server=localhost; user=root;database=factura;password=1234;";
+
+        public Usuario Autenticar(Login login)
+
+        {
+            Usuario user = null;
+
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT * FROM usuario WHERE CodigoUsuario=  @CodigoUsuario AND Contraseña=@Contraseña;");
+
+                using (MySqlConnection _conexion=new MySqlConnection(cadena))
+
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando=new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        comando.Parameters.Add("@CodigoUsuario", MySqlDbType.VarChar, 50).Value = login.CodigoUsuario;
+                        comando.Parameters.Add("@Contrasena", MySqlDbType.VarChar, 50).Value = login.Contraseña;
+
+                        MySqlDataReader dr = comando.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            user = new Usuario();
+
+                            user.CodigoUsuario = dr["CodigoUsuario"].ToString();
+                            user.Nombre = dr["Nombre"].ToString();
+                            user.Contraseña = dr["Contrasena"].ToString();
+                            user.Correo = dr["Correo"].ToString();
+                            user.Rol = dr["Rol"].ToString();
+                            user.FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"]);
+                            user.EstaActivo = Convert.ToBoolean(dr["EstaActivo"]);
+                            if (dr["Foto"].GetType() != typeof(DBNull))
+                            {
+                                user.Foto = (byte[])dr["Foto"];
+                            }
+                        }
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            return user;
+        }
+
+        public bool Insertar(Usuario user)
+        {
+            bool inserto = false;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" INSERT INTO usuario VALUES ");
+                sql.Append(" (@CodigoUsuario, @Nombre, @Contrasena, @Correo, @Rol, @Foto, @FechaCreacion, @EstaActivo); ");
+                using (MySqlConnection _conexion = new MySqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando = new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        comando.Parameters.Add("@CodigoUsuario", MySqlDbType.VarChar, 50).Value = user.CodigoUsuario;
+                        comando.Parameters.Add("@Nombre", MySqlDbType.VarChar, 50).Value = user.Nombre;
+                        comando.Parameters.Add("@Contrasena", MySqlDbType.VarChar, 80).Value = user.Contraseña;
+                        comando.Parameters.Add("@Correo", MySqlDbType.VarChar, 45).Value = user.Correo;
+                        comando.Parameters.Add("@Rol", MySqlDbType.VarChar, 20).Value = user.Rol;
+                        comando.Parameters.Add("@Foto", MySqlDbType.LongBlob).Value = user.Foto;
+                        comando.Parameters.Add("@FechaCreacion", MySqlDbType.DateTime).Value = user.FechaCreacion;
+                        comando.Parameters.Add("@EstaActivo", MySqlDbType.Bit).Value = user.EstaActivo;
+                        comando.ExecuteNonQuery();
+                        inserto = true;
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+            return inserto;
+        }
+
+
+        public bool Editar(Usuario user)
+        {
+            bool edito = false;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" UPDATE usuario SET ");
+                sql.Append(" Nombre = @Nombre, Contrasena = @Contrasena, Correo = @Correo, Rol = @Rol, Foto = @Foto, EstaActivo = @EstaActivo ");
+                sql.Append(" WHERE CodigoUsuario = @CodigoUsuario; ");
+
+                using (MySqlConnection _conexion = new MySqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (MySqlCommand comando = new MySqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = CommandType.Text;
+                        comando.Parameters.Add("@CodigoUsuario", MySqlDbType.VarChar, 50).Value = user.CodigoUsuario;
+                        comando.Parameters.Add("@Nombre", MySqlDbType.VarChar, 50).Value = user.Nombre;
+                        comando.Parameters.Add("@Contrasena", MySqlDbType.VarChar, 80).Value = user.Contraseña;
+                        comando.Parameters.Add("@Correo", MySqlDbType.VarChar, 45).Value = user.Correo;
+                        comando.Parameters.Add("@Rol", MySqlDbType.VarChar, 20).Value = user.Rol;
+                        comando.Parameters.Add("@Foto", MySqlDbType.LongBlob).Value = user.Foto;
+                        comando.Parameters.Add("@EstaActivo", MySqlDbType.Bit).Value = user.EstaActivo;
+                        comando.ExecuteNonQuery();
+                        edito = true;
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+            }
+            return edito;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
